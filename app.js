58bfd4165ffee3fd14555e704416d224f6554b7b
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-02 08:17:29 +0530

    feat:day11 assignment

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
+const express = require('express');
 const path = require('path');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
+//const productRoutes = require('./routes/products');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use(productRoutes);
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});

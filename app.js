14367c04b01a8c0891647044d2b091cd96fcddb1
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-06 12:43:23 +0530

    feat:day-12 implementation

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require('express');
 const path = require('path');
 
-//const productRoutes = require('./routes/products');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(productRoutes);
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+app.listen(3000);

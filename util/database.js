3de2bcd92b19599f700cad3df5351b5e9ff9a25d
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-06 14:22:34 +0530

    feat:day-14 inprogress

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Root@1234'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Root@1234'
+// });
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root','Root@1234',{
+    dialect:'mysql',
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+module.exports = sequelize;

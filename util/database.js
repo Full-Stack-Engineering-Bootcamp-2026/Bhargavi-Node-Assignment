af8b10591752c7d3bf636a709014c45f2a9c828a
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-09 22:40:49 +0530

    feat:Day15 updates

--- util/database.js
+++ util/database.js
@@ -1,3 +1,5 @@
+//------------------------mysql
+
 // const mysql = require('mysql2');
 
 // const pool = mysql.createPool({
@@ -6,13 +8,50 @@
 //     database: 'node-complete',
 //     password: 'Root@1234'
 // });
-
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete','root','Root@1234',{
-    dialect:'mysql',
-    host:'localhost'
-});
-
 // module.exports = pool.promise();
-module.exports = sequelize;
+
+
+//===================sequelize
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('node-complete','root','Root@1234',{
+//     dialect:'mysql',
+//     host:'localhost'
+// });
+
+// 
+// module.exports = sequelize;
+
+//==================================mongodb
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Bhargavi:Bhavi%409@cluster0.adw0uqg.mongodb.net/shop?appName=Cluster0')
+        .then(
+            client => {
+                console.log('Connected!');
+                _db=client.db();
+                callback(client);
+            }).catch(
+                err => {
+                    console.log(err);
+                    throw err;
+                });
+};
+
+const getDb=()=>{
+    if(_db){
+        return _db;
+    }
+    throw 'No database found';
+};
+
+// exports.mongoConnect=mongoConnect;
+// exports.getDb=getDb;
+
+module.exports = { mongoConnect, getDb };
+
+//mongodb+srv://Bhargavi:<db_password>@cluster0.adw0uqg.mongodb.net/?appName=Cluster0

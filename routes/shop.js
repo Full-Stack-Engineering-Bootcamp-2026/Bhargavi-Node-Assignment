3b5268cd0d3882251de83873fc82dbeaed84f2b5
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-10 08:26:33 +0530

    feat:day-17 Assignment

--- routes/shop.js
+++ routes/shop.js
@@ -8,5 +8,5 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
- router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 module.exports = router;

ed4de464c82e2caab56094048f6e198710b9d698
Bhargavi Thorat <bhargavi.thorat@mindbowser.com>
2026-04-10 08:12:23 +0530

    feat: day16 CURD mongoDB

--- models/user.js
+++ models/user.js
@@ -1,16 +1,31 @@
-const Sequelize = require('sequelize');
+const getDb=require('../util/database').getDb
+const mongodb=require('mongodb')
+const ObjectId=mongodb.ObjectId
+class User{
+    constructor(username,email){
+            this.name=username
+            this.email=email
+    }
+    save(){ 
+      //Save
+        const db=getDb()
+       return  db.collection('users').insertOne(this)
 
-const sequelize = require('../util/database');
+    }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+    //Find by ID
+    static findById(userId){
 
-module.exports = User;
+        const db=getDb()
+        return db.collection('users').findOne({_id:new ObjectId(userId)})
+        .then(user=>{
+            console.log(user)
+            return user
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+
+    }
+}
+module.exports=User;
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,22 @@
 const path = require('path');
-
 const express = require('express');
-
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
+
+//       /admin/add-product-- to get
 router.get('/add-product', adminController.getAddProduct);
+
+//          /admin/products-- to get
 router.get('/products', adminController.getProducts);
+
+//          /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+ router.get('/edit-product/:productId',adminController.getEditProduct)
 
-router.post('/edit-product', adminController.postEditProduct);
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/edit-product',adminController.postEditProduct)
+
+//          router.post('/delete-product',adminController.postDeleteProduct)
+
 module.exports = router;
